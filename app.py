81277a74bf0b51b3637ae025384c88602e3100bb
Alan Meigs <drakosfire@gmail.com>
2024-08-03 17:26:52 -0600

    calling search function within apicall

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import gradio as gr
 import time
-import twelvelabs
+import apicall
+from apicall import SearchResult
 
 
 sysprompt = "Hi! Tell me what kind of Hidden Gem you'd like to find."
-with gr.Blocks() as app:
+with gr.Blocks() as demo:
     chatbot = gr.Chatbot()
     msg = gr.Textbox()
     clear = gr.ClearButton([msg, chatbot])
 
     def respond(message, chat_history):
-        bot_message = twelvelabs.chatbot_response(message)
+        bot_message = apicall.search(message)
         chat_history.append((message, bot_message))
         time.sleep(2)
         return "", chat_history
@@ -18,4 +19,4 @@ with gr.Blocks() as app:
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
 
 if __name__ == "__main__":
-    app.launch()
+    demo.launch()
