38523575060bb41d280dc9fb109f0e69599c92e9
Alan Meigs <drakosfire@gmail.com>
2024-08-03 18:43:29 -0600

    Adding tuples of returns from search call to twelve labs

--- app.py
+++ app.py
@@ -6,13 +6,20 @@ from apicall import SearchResult
 
 sysprompt = "Hi! Tell me what kind of Hidden Gem you'd like to find."
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
+    chatbot = gr.Chatbot(type="tuples")
     msg = gr.Textbox()
     clear = gr.ClearButton([msg, chatbot])
 
     def respond(message, chat_history):
         bot_message = apicall.search(message)
-        chat_history.append((message, bot_message))
+        print(f"Bot Message = {bot_message}")
+        print(f"Bot Message 0 = {bot_message[0]}")
+        print(f"Bot Message 1 = {bot_message[1]}")
+        thumbnail = gr.Image(bot_message[0])
+        video = gr.Video(bot_message[1])
+        text = bot_message[2]
+        chat_history.append([(message,text)])
+        chat_history.append([ (thumbnail, video)])
         time.sleep(2)
         return "", chat_history
 
